5a1faad5b547b0c0ed5749f80efea264b86ba7d7
samuelcomputacao <samuel25101998@gmail.com>
2019-09-14 10:45:55 -0300

    validação de email

--- src/services/validation.js
+++ src/services/validation.js
@@ -1,7 +1,7 @@
 function validaEmail(email){
     let retorno = true;
     let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
-    if(reg.test(email) === false){
+    if(email.length>0 && reg.test(email) === false){
         retorno = false;
     }
     return retorno;
