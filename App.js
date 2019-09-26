a64be92ec1d4a13e42a18bce482542979f80d9db
samuelcomputacao <samuel25101998@gmail.com>
2019-09-25 23:39:12 -0300

    verificando conexão com a internet

--- App.js
+++ App.js
@@ -6,9 +6,31 @@
  * @flow
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import Routes from "./src/routes";
+import NetInfo from "@react-native-community/netinfo";
+import { Alert,BackHandler} from 'react-native';
 
 export default function App() {
-  	return <Routes />
+	const[connected,setIsConnected] = useState(false);
+
+	NetInfo.fetch().then(state => {
+		let conn = state.isConnected;
+		setIsConnected(state.isConnected);
+		if(!conn){
+			Alert.alert(
+                'O aplicativo necessita de internet.',
+                'Confira a sua conexão com a internet e tente novamente.',
+                    [
+                    {text: 'OK',onPress:_=>{
+						BackHandler.exitApp();
+					}},
+                  ],
+                {cancelable: false},
+              );	
+		}
+		
+	});
+
+  	return connected && <Routes />
 }
