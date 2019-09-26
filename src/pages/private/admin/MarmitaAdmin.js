4061bde7dabec6e40d7f6ad0dc8f1b6bd4baed95
samuelcomputacao <samuel25101998@gmail.com>
2019-09-25 21:52:28 -0300

    iniciando tela de marmita

--- src/pages/private/admin/MarmitaAdmin.js
+++ src/pages/private/admin/MarmitaAdmin.js
@@ -1,10 +1,11 @@
-import React, { useState,Component } from 'react';
+import React, { useState,Component,useEffect} from 'react';
 import {
     View,
     Text,
     StyleSheet,
     FlatList,
-    ActivityIndicator
+    ActivityIndicator,
+    TouchableOpacity
 } from 'react-native'
 
 import { styles } from '../../../styles/styles';
@@ -12,100 +13,157 @@ import MenuButton from '../MenuButton';
 import api from '../../../services/api';
 import {find} from '../../../services/banco';
 import {USER_CURRENTY} from '../../../services/key'
+import IconMaterial from 'react-native-vector-icons/FontAwesome';
 
 const baseURL = 'https://api.github.com';
 const searchTerm = 'react';
 const perPage = 20;
 
-export default class MarmitaAdmin extends Component {
+// export default class MarmitaAdmin extends Component {
 
-    constructor(props){
-        super(props);
-        this.loadRepositories();
-    }
+//     constructor(props){
+//         super(props);
+//         this.loadRepositories();
+//     }
     
-    state = {
-        data: [],
-        loading: false,
-      };
+//     state = {
+//         data: [],
+//         loading: false,
+//       };
     
-      componentDidMount() {
-        this.loadRepositories();
-      }
+//       componentDidMount() {
+//         this.loadRepositories();
+//       }
     
-      loadRepositories = async () => {
-        if (this.state.loading) return;
+//       loadRepositories = async () => {
+//         if (this.state.loading) return;
     
-        this.setState({ loading: true });
-        let usuario = await find(USER_CURRENTY);
-        //const response = await fetch(`${baseURL}/search/repositories?q=${searchTerm}&per_page=${perPage}&page=${page}`);
-        const response = await api.get('/protegido/marmita/lista',{ headers: {Authorization: usuario.token,}}) ;
-        this.setState({
-          data: [ ...this.state.data, ...response.data],
-          loading: false,
-        });
-      }
+//         this.setState({ loading: true });
+//         let usuario = await find(USER_CURRENTY);
+//         //const response = await fetch(`${baseURL}/search/repositories?q=${searchTerm}&per_page=${perPage}&page=${page}`);
+//         const response = await api.get('/protegido/marmita/lista',{ headers: {Authorization: usuario.token,}}) ;
+//         this.setState({
+//           data: [ ...this.state.data, ...response.data],
+//           loading: false,
+//         });
+//       }
   
+//     renderItem = ({ item }) => (
+//       <View style={mystyles.listItem}>
+//         <Text>{item.tipoMarmita}</Text>
+//       </View>
+//     );
+
+//     renderFooter = () => {
+//         if (!this.state.loading) return null;
+//         return (
+//           <View style={mystyles.loading}>
+//             <ActivityIndicator />
+//           </View>
+//         );
+//       };
+  
+//     render() {
+//       return (
+//         <View style={ styles.mainContainer }>
+//             <MenuButton navigation={this.props.navigation}/>
+//             <FlatList
+//             style={{ marginTop: 50 }}
+//             contentContainerStyle={mystyles.list}
+//             data={this.state.data}
+//             renderItem={this.renderItem}
+//             keyExtractor={item => item.idMarmita}
+//             />
+//         </View>
+//       );    
+//     }
+//   }
+const MarmitaAdmin = (props) => {
+
+    const[data,setData] = useState([]);
+    const[loading,setLoading] = useState(false);
+    const[usuario,setUsuario] = useState({});
+
+    useEffect(() => {
+        loadRepositories();
+      }, []);
+
+    loadRepositories = async () => {
+        setLoading(true);
+        let usuario = await find(USER_CURRENTY);
+         //const response = await fetch(`${baseURL}/search/repositories?q=${searchTerm}&per_page=${perPage}&page=${page}`);
+        const response = await api.get('/protegido/marmita/lista',{ headers: {Authorization: usuario.token,}});
+        setData(response.data);
+        setLoading(false);
+    }
+     
     renderItem = ({ item }) => (
       <View style={mystyles.listItem}>
         <Text>{item.tipoMarmita}</Text>
       </View>
     );
-
+    
     renderFooter = () => {
-        if (!this.state.loading) return null;
+        if (!loading) return null;
         return (
           <View style={mystyles.loading}>
             <ActivityIndicator />
           </View>
         );
       };
-  
-    render() {
-      return (
+
+    
+
+    return (
+       
         <View style={ styles.mainContainer }>
-            <MenuButton navigation={this.props.navigation}/>
-            <FlatList
-            style={{ marginTop: 50 }}
-            contentContainerStyle={mystyles.list}
-            data={this.state.data}
-            renderItem={this.renderItem}
-            keyExtractor={item => item.idMarmita}
+            <MenuButton navigation={props.navigation}/>
+            <View style={ styles.mainContainer }>
+             <FlatList
+             style={{ marginTop: 50 }}
+             contentContainerStyle={mystyles.list}
+             data={data}
+             renderItem={renderItem}
+             keyExtractor={item => item.idMarmita}
             />
+         </View>
+         <TouchableOpacity
+            style={{
+                borderWidth:1,
+                borderColor:'rgba(0,0,0,0.2)',
+                alignItems:'center',
+                justifyContent:'center',
+                width:70,
+                position: 'absolute',                                          
+                bottom: 25,                                                    
+                right: 25,
+                height:70,
+                backgroundColor:'#fff',
+                borderRadius:100,
+                }}
+            >
+           <IconMaterial
+            name='plus'
+            size={20}
+            color='black'
+            style={ styles.iconsDrawer }
+            />
+            </TouchableOpacity>
         </View>
-      );
-    }
-  }
-// const MarmitaAdmin = (props) => {
+    )
+}
 
-//     const[list,setList] = useState([]);
-
-//     async function buscaLista(){
-//         let usuario = await find(USER_CURRENTY);
-//         try{
-//             let lista = await api.get('/protegido/marmita/lista',{ headers: {
-//                 Authorization: usuario.token,
-//             }}); 
-//             setList(lista);
-//             ToastAndroid.show(error.response.data['message'],ToastAndroid.SHORT);   
-//         } catch (e){
-//             ToastAndroid.show("show",ToastAndroid.SHORT);
-//         }
-//     }
-//     renderItem = ({ item }) => (
-//         <View style={mystyles.listItem}>
-//           <Text>{item.valor}</Text>
-//         </View>
-//       );    
-
-//     return (
-//         <View style={ styles.mainContainer }>
-//             <MenuButton navigation={props.navigation}/>
-//             <Text style={{alignSelf: 'center'}}>Marmitas</Text>
-            
-//         </View>
-//     )
-// }
+MarmitaAdmin.navigationOptions = {
+    drawerLabel: 'Marmitas',
+    drawerIcon:({focused, tintColor}) => (
+        <IconMaterial
+            name='cutlery'
+            size={20}
+            color='black'
+            style={ styles.iconsDrawer }
+        />
+    )
+}
 
 const mystyles = StyleSheet.create({
     list: {
@@ -124,4 +182,4 @@ const mystyles = StyleSheet.create({
       }
   });
 
-// export default MarmitaAdmin;
+export default MarmitaAdmin;
