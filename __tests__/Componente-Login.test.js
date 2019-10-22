9cade044467712472ea77d59f0f5b31228b67e47
Rickecr <richelton14@gmail.com>
2019-10-22 00:00:10 -0300

    Reiniciando com testes

--- __tests__/Componente-Login.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Login from "../src/pages/public/Login";
-
-import renderer from 'react-test-renderer';
-
-it('Testando Criação do Componente Login', () => {
-    const tree = renderer.create(<Login />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
