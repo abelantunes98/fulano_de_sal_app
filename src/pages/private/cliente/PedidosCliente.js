c2afc6e9c96631133ffed2508a66f648b529a34c
Abel Antunes <34608001+abelantunes98@users.noreply.github.com>
2019-10-23 14:56:59 -0300

    Atualizando status de pedido conforme aceito pelo admin.

--- src/pages/private/cliente/PedidosCliente.js
+++ src/pages/private/cliente/PedidosCliente.js
@@ -3,6 +3,8 @@ import {
     View,
     Text,
     FlatList,
+    ToastAndroid,
+    Alert,
     TouchableOpacity,
     StyleSheet,
 } from 'react-native'
@@ -11,148 +13,156 @@ import MenuButton from '../MenuButton';
 import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons';
 import IconButton from 'react-native-vector-icons/FontAwesome';
 import { Card, Button } from 'react-native-elements';
+import { find } from '../../../services/banco';
+import api from '../../../services/api';
+import { USER_CURRENTY } from '../../../services/key';
+
 
 const PedidosCliente = (props) => {
 
     const [data, setData] = useState([]);
 
     useEffect(() => {
-		loadRepositories();
+        loadRepositories();
     }, []);
-    
+
     loadRepositories = async () => {
-        let dat = [{data: '07/10/2019', idProduto: 'test', preco: 5.0, status: 'ok'}, 
-        {data: '08/10/2019', idProduto: 'test2', preco: 5.0, status: 'pendente'},
-        {data: '08/10/2019', idProduto: 'test3', preco: 5.0, status: 'erro'}, 
-        {data: '07/10/2019', idProduto: 'test4', preco: 5.0, status: 'ok'}, 
-        {data: '08/10/2019', idProduto: 'test5', preco: 5.0, status: 'pendente'},
-        {data: '08/10/2019', idProduto: 'test6', preco: 5.0, status: 'erro'}];
+        // Para não listar nada caso não tenha pedidos.
+        let dat = [];
+        try {
+            let userDados = await find(USER_CURRENTY);
+            const response = await api.get('/protegido/pedido/listarCliente/',
+                { headers: { Authorization: userDados.token },
+                  params:  { email: userDados.email }});
+
+            if (response.status == 200) {
+                dat = response.data.pedidos;
+            }
+        } catch {
+            ToastAndroid.show('Não foi possível carregar seus pedidos!', ToastAndroid.SHORT);
+        }
         setData(dat);
-	};
+    };
 
     function defineIconeStatus(status) {
-        if(status == 'ok') {
+        if (status) {
             return 'check-circle-o';
-        } else if(status == 'pendente'){
-            return 'question-circle';
         } else {
-            return 'exclamation-circle';
+            return 'question-circle';
         }
     }
 
     function defineCorStatus(status) {
-        if(status == 'ok') {
+        if (status) {
             return '#32CD32';
-        } else if(status == 'pendente'){
-            return '#FFFF00';
         } else {
-            return '#8B0000';
+            return '#FFFF00';
         }
     }
 
     renderItem = ({ item }) => (
-		<Card containerStyle={styles.listItem}>
-			<View>
-				<View style={styles.buttons}>
-					<Button 
-						buttonStyle={styles.button}
-						icon={
-							<IconButton
-								name='expand'
-								size={15}
-								color='#000'
-								style={styles.iconsDrawer}
-							/>
-						}
-					/>
-				</View>
-                <View style={styles.statusPosition}>
-                    <Button 
-						buttonStyle={styles.status}
-						icon={
-							<IconButton
-								name={defineIconeStatus(item.status)} 
-								size={70}
-								color={defineCorStatus(item.status)}
-								style={styles.iconsDrawer}
-							/>
-						}
-					/>
+        <Card containerStyle={styles.listItem}>
+            <View>
+                <View style={styles.buttons}>
+                    <Button
+                        buttonStyle={styles.button}
+                        icon={
+                            <IconButton
+                                name='expand'
+                                size={15}
+                                color='#000'
+                                style={styles.iconsDrawer}
+                            />
+                        }
+                    />
                 </View>
-				<View>
-					<Text  style={styles.dados}>{item.data}</Text>
-                    <Text  style={styles.dados}>Valor: R$ {item.preco}</Text>
-				</View>
-			</View>
-		</Card>
-	);
+                <View style={styles.statusPosition}>
+                    <Button
+                        buttonStyle={styles.status}
+                        icon={
+                            <IconButton
+                                name={defineIconeStatus(item.confirmado)}
+                                size={70}
+                                color={defineCorStatus(item.confirmado)}
+                                style={styles.iconsDrawer}
+                            />
+                        }
+                    />
+                </View>
+                <View>
+                    <Text style={styles.dados}>{item.data}</Text>
+                    <Text style={styles.dados}>Valor: R$ {item.marmita.valor}</Text>
+                </View>
+            </View>
+        </Card>
+    );
 
     return (
         <View style={styles.mainContainer}>
             <MenuButton navigation={props.navigation} title='Pedidos' />
-            <View style={{paddingBottom:70}}>
+            <View style={{ paddingBottom: 70 }}>
                 <FlatList
-               	   	style={{ marginTop: 10 }}
+                    style={{ marginTop: 10 }}
                     contentContainerStyle={styles.list}
                     data={data}
                     renderItem={renderItem}
-                    keyExtractor={item => item.idProduto.toString()}
+                    keyExtractor={item => item.idPedido.toString()}
                 />
             </View>
-                <TouchableOpacity 
-                    style={styles.floatButton}
-                    onPress={()=>{props.navigation.navigate('Novo')}}
-                >
-                    <IconButton
-                        name='plus'
-                        size={20}
-                        color='#ffffff'
-                        style={ styles.iconsDrawer }
-                    />
-                </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.floatButton}
+                onPress={() => { props.navigation.navigate('Novo') }}
+            >
+                <IconButton
+                    name='plus'
+                    size={20}
+                    color='#ffffff'
+                    style={styles.iconsDrawer}
+                />
+            </TouchableOpacity>
         </View>
     )
 }
 
 PedidosCliente.navigationOptions = {
     drawerLabel: 'Pedidos',
-    drawerIcon:({focused, tintColor}) => (
+    drawerIcon: ({ focused, tintColor }) => (
         <IconMaterial
             name='food'
             size={20}
             color='black'
-            style={ styles.iconsDrawer }
+            style={styles.iconsDrawer}
         />
     )
 }
 
 const styles = StyleSheet.create({
     list: {
-		paddingTop: 10,
-		paddingHorizontal: 16
+        paddingTop: 10,
+        paddingHorizontal: 16
     },
     dados: {
         textAlign: 'right',
         fontWeight: 'bold',
         marginTop: 0
     },
-	button: {
-		backgroundColor: '#FFF',
-		borderRadius: 100,
-		height: 30,
-		width: 30,
-		marginLeft: 18,
-		alignItems: 'center',
-		justifyContent: 'center',
+    button: {
+        backgroundColor: '#FFF',
+        borderRadius: 100,
+        height: 30,
+        width: 30,
+        marginLeft: 18,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-	buttons: {
-		flexDirection: 'row',
+    buttons: {
+        flexDirection: 'row',
         justifyContent: 'flex-end',
     },
     status: {
         flex: 1,
-		height: 15,
-		width: 80,
+        height: 15,
+        width: 80,
         backgroundColor: '#FFF'
     },
     statusPosition: {
@@ -160,30 +170,30 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'flex-start',
     },
-	listItem: {
-		borderRadius: 10,
-		backgroundColor: '#FFF',
-        borderColor:'#000',
-	},
+    listItem: {
+        borderRadius: 10,
+        backgroundColor: '#FFF',
+        borderColor: '#000',
+    },
     mainContainer: {
         flex: 1,
         backgroundColor: '#ffffff',
     },
     floatButton: {
-		borderWidth: 1,
-		borderColor: 'rgba(0,0,0,0.2)',
-		alignItems: 'center',
-		justifyContent: 'center',
-		width: 70,
-		position: 'absolute',
-		bottom: 10,
-		right: 25,
-		height: 70,
-		backgroundColor: '#0f6124',
-		borderRadius: 100
-	},
+        borderWidth: 1,
+        borderColor: 'rgba(0,0,0,0.2)',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 70,
+        position: 'absolute',
+        bottom: 10,
+        right: 25,
+        height: 70,
+        backgroundColor: '#0f6124',
+        borderRadius: 100
+    },
     iconsDrawer: {
-		paddingRight: 2
+        paddingRight: 2
     }
 });
 
